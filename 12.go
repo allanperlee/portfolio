42b53e7fe7feac3cd40ab961281eab04bdc5ec39
allanperlee <48000848+allanperlee@users.noreply.github.com>
2020-05-16 15:05:56 -0700

    Solves problem 12 in Go. Three helper functions with one boolean checking for triangle numbers, the second counts the divisors for any natural number, and the third that finds the number that adds up to the triangular number. For instance for triangle number 10 returns 4. The rest are while loops that start from the first number that contains 500 divisors using prime factorization. The startHere var and lastItem var are incremented until the first triangle number with over 500 divisors is found.

---
+++ 12.go
@@ -0,0 +1,55 @@
+package main
+
+import( "fmt";
+	"math")
+
+func isTriangle(natural int) bool{
+	var otherNatural float64 = float64(natural)
+	var firstStep int = int(math.Sqrt(2*otherNatural))
+	return  int(0.5 * float64(firstStep) * (float64(firstStep+1))) == natural
+}
+
+func divisors(natural int) int{
+	var counter int = 0
+	var end int = int(math.Sqrt(float64(natural)))
+	var i int
+
+	for i = 1; i < end+1; i++{
+		if natural % i == 0{
+			counter += 1
+			if i != natural/i{
+				counter += 1
+		}
+		}
+	}
+	return counter
+}
+
+func continued(natural int) int{
+	if isTriangle(natural){
+		return int(math.Sqrt(float64(2*natural))) + 1
+	} else {
+		return 0
+	}
+}
+
+
+
+func main(){
+	var startHere int = int(math.Pow(2, 4)) * int(math.Pow(3, 4)) * int(math.Pow(5, 4)) * 7 * 11
+	
+	for !isTriangle(startHere){
+		startHere ++
+	}
+
+	var lastItem int = continued(startHere)
+
+	for divisors(startHere) <= 500{
+		startHere += lastItem
+		lastItem ++
+	}
+
+	fmt.Println(startHere)
+}
+
+//prints 76576500
